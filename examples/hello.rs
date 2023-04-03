721438f40380cb0fd1ca9e468d7dd9acf8df9a45
Raiki Tamura <tamaron1203@gmail.com>
2023-04-03 18:12:21 +0900

    Update hello.rs

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,9 +1,10 @@
 extern "C" {
-    fn printf(s: &str) -> i32;
+    fn puts(s: &str) -> i32;
 }
 
 fn main() -> () {
     unsafe {
-        printf("Hello world!\n");
+        ();
+        puts("Hello world!");
     };
 }

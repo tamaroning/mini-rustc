f4025d54821968a0a595aa1135871785e0d0e9e8
Raiki Tamura <tamaron1203@gmail.com>
2023-04-05 12:10:54 +0900

    [llvm] add unary and binary

--- src/backend_llvm/mod.rs
+++ src/backend_llvm/mod.rs
@@ -1,4 +1,4 @@
-use crate::ast::{self, Crate, Expr, ExprKind, Func, ItemKind, LetStmt, Stmt, StmtKind};
+use crate::ast::{self, Block, Crate, Expr, ExprKind, Func, ItemKind, LetStmt, Stmt, StmtKind};
 use crate::middle::ty::Ty;
 use crate::middle::Ctxt;
 
@@ -34,6 +34,7 @@ impl<'a> Codegen<'a> {
         match ty {
             Ty::Unit => LLTy::Void,
             Ty::I32 => LLTy::I32,
+            Ty::Bool => LLTy::I8,
             _ => panic!(),
         }
     }
@@ -41,16 +42,16 @@ impl<'a> Codegen<'a> {
     fn go(&mut self, krate: &'a Crate) -> Result<(), ()> {
         println!(r#"target triple = "x86_64-unknown-linux-gnu""#);
         println!("");
-        self.codegen_crate(krate)?;
+        self.gen_crate(krate)?;
         // TODO: literals
         Ok(())
     }
 
-    fn codegen_crate(&mut self, krate: &'a Crate) -> Result<(), ()> {
+    fn gen_crate(&mut self, krate: &'a Crate) -> Result<(), ()> {
         for item in &krate.items {
             match &item.kind {
                 ItemKind::Func(func) => {
-                    self.codegen_func(func)?;
+                    self.gen_func(func)?;
                 }
                 ItemKind::Struct(_) => (),
                 ItemKind::ExternBlock(_) => (),
@@ -59,7 +60,7 @@ impl<'a> Codegen<'a> {
         Ok(())
     }
 
-    fn codegen_func(&mut self, func: &'a Func) -> Result<(), ()> {
+    fn gen_func(&mut self, func: &'a Func) -> Result<(), ()> {
         // do not generate code for the func if it does not have its body
         if func.body.is_none() {
             return Ok(());
@@ -70,9 +71,14 @@ impl<'a> Codegen<'a> {
             self.ty_to_llty(&func.ret_ty).to_string(),
             func.name.symbol
         );
-        if let Some(body) = &func.body {
-            for stmt in &body.stmts {
-                self.codegen_stmt(stmt)?;
+        let body_res = self.gen_block(func.body.as_ref().unwrap())?;
+
+        match body_res {
+            Some(LLReg { reg, llty }) => println!("\tret {} {}", llty.to_string(), reg),
+            None => {
+                if self.ty_to_llty(&func.ret_ty) == LLTy::Void {
+                    println!("\tret void");
+                }
             }
         }
         //println!("\tret void");
@@ -80,46 +86,121 @@ impl<'a> Codegen<'a> {
         Ok(())
     }
 
-    fn codegen_stmt(&mut self, stmt: &'a Stmt) -> Result<(), ()> {
+    fn gen_block(&mut self, block: &'a Block) -> Result<Option<LLReg>, ()> {
+        let mut last_stmt_res = None;
+        for stmt in &block.stmts {
+            last_stmt_res = self.gen_stmt(stmt)?;
+        }
+        Ok(last_stmt_res)
+    }
+
+    fn gen_stmt(&mut self, stmt: &'a Stmt) -> Result<Option<LLReg>, ()> {
         println!("; Starts stmt `{}`", stmt.span.to_snippet());
-        let store_kind = match &stmt.kind {
+        let res = match &stmt.kind {
             StmtKind::Semi(expr) => {
-                self.codegen_expr(expr)?;
+                self.gen_expr(expr)?;
+                Ok(None)
             }
-            StmtKind::Expr(expr) => {
-                self.codegen_expr(expr)?;
-            }
+            StmtKind::Expr(expr) => Ok(self.gen_expr(expr)?),
             StmtKind::Let(_) => {
                 // TODO:
+                todo!()
             }
         };
         println!("; Finished stmt `{}`", stmt.span.to_snippet());
-        Ok(store_kind)
+        res
     }
 
-    fn codegen_expr(&mut self, expr: &'a Expr) -> Result<Option<RegAndLLTy>, ()> {
+    fn gen_expr(&mut self, expr: &'a Expr) -> Result<Option<LLReg>, ()> {
         println!("; Starts expr `{}`", expr.span.to_snippet());
         let ret = match &expr.kind {
             ExprKind::NumLit(n) => {
                 let reg = self.get_fresh_reg();
                 let llty = LLTy::I32;
                 println!("\t{reg} = bitcast i32 {} to i32", n);
-                Ok(Some(RegAndLLTy::new(reg, llty)))
+                Some(LLReg::new(reg, llty))
+            }
+            ExprKind::BoolLit(b) => {
+                let n = if *b { 1 } else { 0 };
+                let reg = self.get_fresh_reg();
+                let llty = LLTy::I8;
+                println!("\t{reg} = bitcast i8 {} to i8", n);
+                Some(LLReg { reg, llty })
+            }
+            ExprKind::Unary(unop, inner) => match unop {
+                ast::UnOp::Minus => {
+                    let llreg = self.gen_expr(inner)?.unwrap();
+                    assert!(llreg.llty.is_integer());
+                    let reg = self.get_fresh_reg();
+                    println!("\t{reg} = sub {} 0, {}", llreg.llty.to_string(), llreg.reg);
+                    Some(LLReg::new(reg, llreg.llty))
+                }
+                ast::UnOp::Plus => self.gen_expr(inner)?,
+            },
+            ExprKind::Binary(binop, lhs, rhs) => {
+                let l = self.gen_expr(lhs)?.unwrap();
+                let r = self.gen_expr(rhs)?.unwrap();
+                // checks if rhs and lhs have the same type
+                assert_eq!(self.ctx.get_type(lhs.id), self.ctx.get_type(rhs.id));
+                let rhs_lhs_llty = self.ty_to_llty(&self.ctx.get_type(lhs.id));
+
+                let reg = self.get_fresh_reg();
+                let llty = match binop {
+                    ast::BinOp::Add => {
+                        assert!(rhs_lhs_llty.is_integer());
+                        println!(
+                            "\t{reg} = add {} {}, {}",
+                            rhs_lhs_llty.to_string(),
+                            l.reg,
+                            r.reg
+                        );
+                        rhs_lhs_llty
+                    }
+                    ast::BinOp::Sub => {
+                        assert!(rhs_lhs_llty.is_integer());
+                        println!(
+                            "\t{reg} = sub {} {}, {}",
+                            rhs_lhs_llty.to_string(),
+                            l.reg,
+                            r.reg
+                        );
+                        rhs_lhs_llty
+                    }
+                    ast::BinOp::Mul => {
+                        assert!(rhs_lhs_llty.is_integer());
+                        println!(
+                            "\t{reg} = mul {} {}, {}",
+                            rhs_lhs_llty.to_string(),
+                            l.reg,
+                            r.reg
+                        );
+                        rhs_lhs_llty
+                    }
+                    ast::BinOp::Eq => {
+                        println!("\t{reg} = icmp eq {}, {}", l.reg, r.reg);
+                        self.ty_to_llty(&Ty::Bool)
+                    }
+                    _ => todo!(),
+                };
+                Some(LLReg { reg, llty })
             }
             ExprKind::Return(inner) => {
-                let RegAndLLTy { reg, llty } = self.codegen_expr(inner)?.unwrap();
+                let LLReg { reg, llty } = self.gen_expr(inner)?.unwrap();
                 println!("\tret {} {}", llty.to_string(), reg);
-                Ok(None)
+                None
             }
+            ExprKind::Block(block) => self.gen_block(block)?,
             _ => panic!(),
         };
         println!("; Starts expr `{}`", expr.span.to_snippet());
-        ret
+        Ok(ret)
     }
 }
 
+#[derive(Debug, PartialEq, Eq)]
 enum LLTy {
     Void,
+    I8,
     I32,
 }
 
@@ -127,18 +208,24 @@ impl LLTy {
     fn to_string(&self) -> String {
         match self {
             LLTy::Void => "void".to_string(),
+            LLTy::I8 => "i8".to_string(),
             LLTy::I32 => "i32".to_string(),
         }
     }
+
+    fn is_integer(&self) -> bool {
+        matches!(self, LLTy::I32)
+    }
 }
 
-struct RegAndLLTy {
+#[derive(Debug, PartialEq, Eq)]
+struct LLReg {
     reg: String,
     llty: LLTy,
 }
 
-impl RegAndLLTy {
-    fn new(reg: String, llty: LLTy) -> Self {
-        RegAndLLTy { reg, llty }
+impl LLReg {
+    fn new(llreg: String, llty: LLTy) -> Self {
+        LLReg { reg: llreg, llty }
     }
 }
--- tests_llvm/execute.sh
+++ tests_llvm/execute.sh
@@ -39,4 +39,24 @@ cargo build
 cd tests_llvm
 
 echo "===== Execute Tests ====="
+# return
 assert 0 'fn main() -> i32 { return 0; }'
+assert 0 'fn main() -> i32 { { return 0; } }'
+# func body
+assert 0 'fn main() -> i32 { 0 }'
+assert 0 'fn main() -> i32 { { { 0 } } }'
+assert 10 'fn main() -> i32 { { { 10 } } }'
+# unary
+# Linux only?
+assert 255 'fn main() -> i32 { -1 }'
+assert 254 'fn main() -> i32 { -2 }'
+# numerical literals
+assert 200 'fn main() -> i32 { 100; 200 }'
+assert 3 'fn main() -> i32 { 0; 1; 2; 3 }'
+# boolean literals
+assert 0 'fn main() -> i32 { true; 0 }'
+# binop
+assert 9 'fn main() -> i32 { 4 + 5 }'
+assert 3 'fn main() -> i32 { 10 - 7 }'
+assert 6 'fn main() -> i32 { 2 * 3 }'
+assert 9 'fn main() -> i32 { 11 + 8 * 2 - 3 * (1 + 5) }'
